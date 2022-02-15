705028364ca5a9d0a124c0e779f6031fb59889ad
Ko1baser <astarikov541@gmail.com>
2022-02-15 11:21:32 +0300

    добавил немнога кода

--- first_prog/Program.cs
+++ first_prog/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите любое число");
+int chislo = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("введите другое число для вычислений");
+int prost = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"сумма: {chislo+prost}");
+Console.WriteLine($"разность: {chislo - prost}");
+Console.WriteLine($"произведение: {chislo * prost}");
+Console.WriteLine($"деление: {chislo / prost}");
+Console.WriteLine($"возведение в степень: {Math.Pow(chislo, prost)}");
+Console.WriteLine($"корень числа: {Math.Sqrt(chislo)}");
+
+int[] arr = new int[chislo];
+
+Random rnd = new Random();
+
+for (int i = 0; i < chislo; i++)
+{
+    arr[i] = rnd.Next(0, 100);
+    Console.Write($"{arr[i]}  ");
+}
